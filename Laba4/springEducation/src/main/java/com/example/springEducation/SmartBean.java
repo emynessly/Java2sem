06af4bfcbe0f86d1ed014b20db0e509a4e5ff05d
emynessly <144156159+emynessly@users.noreply.github.com>
2025-05-22 06:04:52 +0300

    Add files via upload

---
+++ Laba4/springEducation/src/main/java/com/example/springEducation/SmartBean.java
@@ -0,0 +1,47 @@
+package com.example.springEducation;
+
+import java.util.Date;
+
+public class SmartBean {
+    private String firstName;
+    private String surname;
+    private Date birthdate;
+
+    public SmartBean(String firstName, String surname, Date birthdate) { // конструктор
+        this.firstName = firstName;
+        this.surname = surname;
+        this.birthdate = birthdate;
+    }
+    public String getFirstName(){ // имя
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getSurname(){ // фамилия
+        return surname;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+
+    public Date getBirthdate(){ // дата рождения
+        return birthdate;
+    }
+    public void setBirthdate(Date birthdate){
+        this.birthdate = birthdate;
+    }
+
+    @Override
+    public String toString(){
+        return "My name is " + firstName + " " + surname + ". I was born at " + birthdate;
+    }
+
+    public void onInit(){
+        System.out.println("I AM THE DICTATOR OF THIS STONE WORLD");
+    }
+    public void onDestroy(){
+        System.out.println("oh no I am stone again");
+    }
+}
---
+++ Laba4/springEducation/src/main/java/com/example/springEducation/SpringEducationApplication.java
@@ -0,0 +1,22 @@
+package com.example.springEducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringEducationApplication {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml"
+		);
+		SmartBean test = context.getBean("student", SmartBean.class);
+		System.out.println(test);
+
+		weaponShower shower = context.getBean("weaponShowerBean", weaponShower.class);
+		shower.show();
+		context.close();
+		//SpringApplication.run(SpringEducationApplication.class, args);
+	}
+}
